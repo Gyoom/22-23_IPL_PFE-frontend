ee773adca0da777ab488cf5c1afee83cbb77888b
Gyoom <guillaume.piette@student.vinci.be>
2022-12-07 14:51:20 +0100

    lest login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
-  {path:'login', component: LoginComponent},
+  {path:'', component: LoginComponent},
   {path:'register', component: RegisterComponent}
 ];
 
