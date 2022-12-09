2c2e26a4c9284f560fe4432410081ae2959717c3
Chmouss <cyprien.lek@hotmail.com>
2022-12-09 01:18:20 +0100

    add model

---
+++ src/app/models/utilisateurs.ts
@@ -0,0 +1,19 @@
+export interface Utilisateur {
+    id: number;
+    nom: string,
+    prenom: string
+}
+
+export const utilisateurs = [
+    {
+        id: 1,
+        nom: 'Nom',
+        prenom: 'Prenom'
+    },
+
+    {
+        id: 2,
+        nom: 'Nom2',
+        prenom: 'Prenom2'
+    }
+];
