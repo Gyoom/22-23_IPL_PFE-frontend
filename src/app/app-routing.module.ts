35cf102616ed007c1f0be12c47b241487a19985b
Gyoom <guillaume.piette@student.vinci.be>
2022-12-09 16:28:14 +0100

    new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  {path:'', component: LoginComponent},
-  {path:'register', component: RegisterComponent}
+  {path:'', redirectTo:'login', pathMatch: 'full'},
+  {path:'login', component: LoginComponent},
+  {path:'register', component: RegisterComponent},
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
