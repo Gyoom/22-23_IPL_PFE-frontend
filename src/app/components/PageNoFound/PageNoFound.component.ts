2e339f089f4af37095a6a734f952b6e98b75f216
Gyoom <guillaume.piette@student.vinci.be>
2022-12-09 17:26:46 +0100

    complete html register

--- src/app/components/PageNoFound/PageNoFound.component.ts
+++ src/app/components/PageNoFound/PageNoFound.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-PageNoFound',
-  templateUrl: "<h1>Error 404</h1>",
+  template: "<h1>Error 404</h1>",
   styleUrls: []
 })
 export class PageNoFoundComponent {
