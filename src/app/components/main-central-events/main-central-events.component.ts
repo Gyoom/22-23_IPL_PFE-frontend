c4e21318b77078007d1228dcf892dca38a8d99ff
Chmouss <cyprien.lek@hotmail.com>
2022-12-13 17:56:35 +0100

    changements

--- src/app/components/main-central-events/main-central-events.component.ts
+++ src/app/components/main-central-events/main-central-events.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { evenements } from 'src/app/models/evenements';
+import { EventService } from 'src/app/services/event.service';
 
 @Component({
   selector: 'app-main-central-events',
@@ -14,9 +15,21 @@ export class MainCentralEventsComponent implements OnInit {
     window.alert('vous avez ajouté cette personne en ami !');
   }*/
 
-  constructor() { }
+  constructor(
+    private eventService : EventService
+  ) { }
 
+  public events = [];
+  public error = '';
   ngOnInit() {
+    this.eventService.getAll().subscribe(
+      data => {
+        this.events = data;
+      },
+      err => {
+        this.error = err.error.message;
+      }
+    );
   }
 
 }
