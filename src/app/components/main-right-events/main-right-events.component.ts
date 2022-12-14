2348b1eb5f06f4844402e353ffb170e76ae65f1d
Gyoom <guillaume.piette@student.vinci.be>
2022-12-14 03:07:33 +0100

    filter & sort personal events

--- src/app/components/main-right-events/main-right-events.component.ts
+++ src/app/components/main-right-events/main-right-events.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from 'src/app/services/event.service';
+import { TokenStorageService } from 'src/app/services/token-storage.service';
 
 @Component({
   selector: 'app-main-right-events',
@@ -9,16 +10,33 @@ import { EventService } from 'src/app/services/event.service';
 export class MainRightEventsComponent implements OnInit {
 
   constructor(
-    private eventService : EventService
+    private eventService : EventService,
+    private tokenStorage: TokenStorageService
   ) { }
 
   public events = [];
+  public noEvents = 0;
   public error = '';
+  private test = '';
 
   ngOnInit() {
     this.eventService.getAll().subscribe(
       data => {
         this.events = data;
+        this.events = this.events.filter(event => event["_fields"][0]["properties"]["username"] == this.tokenStorage.getUser().username); // ok
+
+        this.events = this.events.filter(event => new Date(event["_fields"][0]["properties"]["ending_date"]) > new Date()); // ok
+        this.events = this.events.sort(function compare(a, b) {
+          if ( new Date(a["_fields"][0]["properties"]["starting_date"]) < new Date(b["_fields"][0]["properties"]["starting_date"]))
+             return -1;
+          if (new Date(a["_fields"][0]["properties"]["starting_date"]) > new Date(b["_fields"][0]["properties"]["starting_date"]) )
+             return 1;
+          return 0;
+        }); // ok
+
+       this.test = new Date().toUTCString();
+       console.log(this.test);
+        this.noEvents = this.events.length
       },
       err => {
         this.error = err.error.message;
